9c220bb0bdd0b6b7f3aa518df6353fc62204f618
triplepointer <termohimia@gmail.com>
2021-01-23 01:58:39 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        helloFromSSR: 'Hello from SSR',
+    },
+    publicRuntimeConfig: {
+        helloFromSSR: 'Hello from SSR'
+    }
+}
